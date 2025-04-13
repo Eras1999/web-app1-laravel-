ea97e7ff5f6ec4f0b4db5db12b2ac9c6ab0d51c1
Eranda Madusanka <madushankaeranda56@gmail.com>
2025-04-13 12:41:42 +0530

    update1

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::get('/home', function () {
+    return view('pages.home');
 });
 
 Route::get('/dashboard', function () {
